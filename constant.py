9a8abbd17060dd60907e1b143032f4fac4942256
abelsalim <abelcosta.ti@gmail.com>
2022-12-30 14:44:49 -0300

    constantes.

---
+++ constant.py
@@ -0,0 +1,9 @@
+COMUNICADOR = {
+    'Comunicador': 
+    '/usr/bin/nohup /opt/sefaz/cco/bin/runcco-ser.sh > /dev/null 2>&1 &'
+    }
+
+MONITOR = {
+    'Monitor':
+    '/usr/bin/nohup /opt/sefaz/cco/bin/runcco-mon.sh > /dev/null 2>&1 &'
+    }

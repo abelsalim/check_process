23b07297999198616dab32077c45d0ad24389f38
abelsalim <abelcosta.ti@gmail.com>
2023-01-07 05:45:52 -0300

    adicionado a funcionalidade de concorrência com threads.

--- check_async.py
+++ check_async.py
@@ -1,12 +1,12 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python3.9
 
 import subprocess
-from asyncio import run, sleep, gather
+from asyncio import run, sleep, gather, to_thread
 from psutil import process_iter
 from constant import COMUNICADOR, MONITOR
 
 
-async def up_down(**process):
+def up_down(process):
     """
     Função responsável por identificar se o processo em palta está em execução.
     """
@@ -22,9 +22,13 @@ async def main():
     Função principal que executa as verificações/execuções como concorrências.
     """
     while True:
-        await gather(up_down(**COMUNICADOR), up_down(**MONITOR))
+        await gather(
+            to_thread(up_down, MONITOR),
+            to_thread(up_down, COMUNICADOR)
+            )
         await sleep(10)
 
 
 if __name__ == '__main__':
     run(main())
+
